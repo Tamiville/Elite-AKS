de48322f595524f621d9d806561e5bddbf66634a
tamiville <tamiville01@outlook.com>
2022-11-09 00:56:36 +0200

    created acr + azure-kubernetes-cluster + a simple app deployment

--- config.tf
+++ config.tf
@@ -16,6 +16,11 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+
+  subscription_id = "60313366-264b-451c-96a9-b264760c42e5"
+  tenant_id       = "4df27d8c-7680-444b-8aec-522f96e17501"
+  client_id       = "a4c37805-48e6-4d6b-b54b-1d272a17b763"
+  client_secret   = "Poy8Q~7zD~IQRIaczTjicJq2k~zRwCOwXSwPedm."
 }
 
 provider "kubernetes" {
